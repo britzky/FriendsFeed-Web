ad1956a228fde967ce192dcec6b8d927035bdf4f
John <johnclapper89@gmail.com>
2023-11-30 18:32:56 -0500

    Added RestaurantList

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { RestaurantCard } from './RestaurantCard';
+import { useRestaurant } from '../context/RestaurantContext';
+import { useFriends } from '../context/FriendContext';
+import { useReview } from '../context/ReviewContext';
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 export const RestaurantList = () => {
+  const { avatars, fetchAvatars, reviews } = useReview();
+  const { restaurants } = useRestaurant();
+  const { friends } = useFriends();
+  const { accessToken } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (restaurants && Array.isArray(restaurants)) {
+      restaurants.forEach((restaurant) => {
+        fetchAvatars(restaurant.id, accessToken);
+      })
+    }
+  }, [restaurants, friends, reviews]);
+
   return (
-    <div>RestaurantList</div>
+    <div>
+      {restaurants.map((restaurant) => (
+        <RestaurantCard
+          key={restaurant.id}
+          name={restaurant.name}
+          image={restaurant.image_url}
+          cuisine={restaurant.categories.map((category) => category.title).join(', ')}
+          rating={restaurant.friend_ratings}
+          friendAvatars={avatars[restaurant.id]}
+          onClick={() => navigate(`/restaurant/${restaurant.id}`)}
+          onReviewClick={() => navigate(`/review/${restaurant.id}`)}
+        />
+      ))
+        }
+    </div>
   )
 }
