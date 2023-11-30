4e5a4d87e1ed7a03ced4d61cb3359f2db51ce75a
John <johnclapper89@gmail.com>
2023-11-30 10:42:52 -0500

    Added RatingsDropdown and StarRating

--- src/components/RatingsDropdown.jsx
+++ src/components/RatingsDropdown.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa'
 
 export const RatingsDropdown = () => {
+  const renderStars = (rating) => {
+    return Array.from({ length: rating }, (_, index) => (
+      <FaStar key={index} className="text-black m-0.5" />
+    ));
+  }
   return (
-    <div>RatingsDropdown</div>
+    <div className="p-2.5">
+      {[1, 2, 3, 4, 5].map((rating) => (
+        <button
+          key={rating}
+          className="flex items-center p-1"
+          onClick={() => onRatingSelect(rating)}
+        >
+          {renderStars(rating)}
+        </button>
+      ))}
+    </div>
   )
 }
