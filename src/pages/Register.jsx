bf31b66470713022ab618bf3196ea4f5f3ebaeca
John <johnclapper89@gmail.com>
2023-12-03 16:36:51 -0500

    Made Register page more responsive

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -39,13 +39,12 @@ export const Register = () => {
   };
 
   return (
-    <div className="flex justify-center min-h-screen w-full my-14">
-      <div className="flex flex-col justify-center w-full px-4 md:max-w-lg">
-        <div className="flex flex-col items-center mx-auto">
+    <div className="flex items-center justify-center h-screen mx-4 md:mx-0">
+      <div className="flex flex-col items-center w-full max-w-md">
           <h1 className="text-5xl font-luckiest text-center text-darkGreen">Friends Feed</h1>
           <h2 className="text-xl my-2 text-center">Discover new restaurants one friend at a time.</h2>
           <input
-            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-96"
+            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-full"
             type="text"
             name="username"
             id="username"
@@ -55,7 +54,7 @@ export const Register = () => {
           <p className="m-2">This is how you'll appear to your friends on Friends Feed.</p>
           {error && error.username && <p className="text-red-500">{error.username}</p>}
           <input
-            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-96"
+            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-full"
             type="password"
             name="password"
             id="password"
@@ -65,7 +64,7 @@ export const Register = () => {
           <p className="m-2">Password must include: 6 to 20 characters</p>
           {error && error.password && <p className="text-red-500">{error.password}</p>}
           <input
-            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-96"
+            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-full"
             type="email"
             name="email"
             id="email"
@@ -75,7 +74,7 @@ export const Register = () => {
           {error && error.email && <p className="text-red-500">{error.email}</p>}
           <input
             type="text"
-            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-96"
+            className="border-2 border-primaryGreen rounded-md p-2 m-2 w-full"
             name="location"
             id="location"
             placeholder="Location"
@@ -84,12 +83,11 @@ export const Register = () => {
           <p className="text-center">Enter your city and state so we can start showing you recommendations nearby.</p>
           {error && error.location && <p className="text-red-500">{error.location}</p>}
           <button
-            className="px-4 py-2 bg-primaryGreen text-white rounded-md hover:bg-secondaryGreen mt-10 w-96"
+            className="px-4 py-2 bg-primaryGreen text-white rounded-md hover:bg-secondaryGreen mt-10 w-full"
             onClick={handleRegistration}
           >
             Continue
           </button>
-        </div>
       </div>
     </div>
   )
