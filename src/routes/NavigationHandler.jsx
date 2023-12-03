2cab7cb50e718d1f48a419ae9ef17bcd59e1f481
John <johnclapper89@gmail.com>
2023-12-03 12:41:00 -0500

    Fixed Navigationn

--- src/routes/NavigationHandler.jsx
+++ src/routes/NavigationHandler.jsx
@@ -1,20 +1,25 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
 export const NavigationHandler = () => {
-    const { isLoggedIn, loading } = useAuth();
+    const { isLoggedIn, loading, inRegistrationFlow } = useAuth();
     const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         if (!loading) {
-          if (isLoggedIn) {
-            navigate('/home');
-          } else {
-            navigate('/');
-          }
+            if (!isLoggedIn && !inRegistrationFlow && location.pathname !== '/') {
+                // If not logged in and not in registration flow, redirect to '/'
+                navigate('/');
+            } else if (isLoggedIn && location.pathname === '/') {
+                // If logged in and on '/', redirect to '/home'
+                navigate('/home');
+            }
+            // No redirection if the user is in the registration flow or already logged in
         }
-      }, [isLoggedIn, loading, navigate]);
+    }, [isLoggedIn, loading, inRegistrationFlow, navigate, location.pathname]);
+
 
       if (loading) {
         return <div>Loading...</div>;
